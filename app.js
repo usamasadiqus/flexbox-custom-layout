3c2c6758f0d4971a675960d5656cb5f7b3267bde
usamasadiqus <usamasadiq029@gmail.com>
2023-06-14 04:42:43 +0500

    update header structure with add date time functionality

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var currentYear = document.getElementById("current-year");
+var currentDateTime = document.getElementById("current-date-time");
 
 var date = new Date();
 
 onload = function () {
   setCurrentYear();
+  setCurrentDate();
 };
 
 function setCurrentYear() {
   currentYear.innerHTML = date.getFullYear();
+}
+
+function setCurrentDate() {
+  currentDateTime.innerHTML = date;
 }
